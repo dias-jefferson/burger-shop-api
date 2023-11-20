ccb5bbf84b7bcd00bd182d8724a023d8ed84d8ba
dias-jefferson <jeff.dias97@gmail.com>
2023-11-20 10:10:01 -0300

    projeto concluído

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ app.use(express.json())
 
 const orders = []
 
+const checkOrdersId = (request, response, next) => {
+    const { id } = request.params
+    
+    const index = orders.findIndex(clientOrder => clientOrder.id === id)
+
+    if(index < 0) {
+        return response.status(404).json({error: "Order not found"})
+    }
+
+    request.orderId = id
+    request.orderIndex = index
+    next()
+}
+
+const checkRouteMethod = (request, response, next) => {
+    const routeType = request.method
+
+    console.log(`${routeType} - /order`)
+    next()
+}
+
+app.use(checkRouteMethod)
+
 app.post('/order', (request, response) => {
     const {order, clientName, price} = request.body
 
@@ -21,56 +44,37 @@ app.get('/order', (request, response) => {
     return response.json(orders)
 })
 
-app.put('/order/:id', (request, response) => {
-    const { id } = request.params
+app.put('/order/:id', checkOrdersId, (request, response) => {
+    const index = request.orderIndex
     const {order, clientName, price, status} = request.body
+    
+    const  id  = request.orderId
     const updatedOrder = {id, order, clientName, price, status}
-    const index = orders.findIndex(clientOrder => clientOrder.id === id)
-
-    if(index < 0) {
-        return response.status(404).json({error: "Order not found"})
-    }
+    
 
     orders[index] = updatedOrder
 
     return response.json(updatedOrder)
 })
 
-app.delete('/order/:id', (request, response) => {
-    const { id } = request.params
+app.delete('/order/:id', checkOrdersId, (request, response) => {
     
-    const index = orders.findIndex(clientOrder => clientOrder.id === id)
-
-    if(index < 0) {
-        response.status(404).json({error: "Order not found"})
-    }
+    const index = request.orderIndex
 
     orders.splice(index, 1)
 
     return response.status(204).json()
 })
 
-app.get('/order/:id', (request, response) => {
-    const { id } = request.params
-
-    const index = orders.findIndex(clientOrder => clientOrder.id === id)
-
-    if(index < 0) {
-        return response.status(404).json({error: "Order not found"})
-    }
+app.get('/order/:id', checkOrdersId, (request, response) => {
+    const  index = request.orderIndex
 
     return response.json(orders[index])
 })
 
-app.patch('/order/:id', (request, response) => {
-    const { id } = request.params
-    
-    const index = orders.findIndex(clientOrder => clientOrder.id === id)
-    
-    
-    if(index < 0) {
-        return response.status(404).json({error: "Order not found"})
-    }
+app.patch('/order/:id', checkOrdersId, (request, response) => {
+
+    const index = request.orderIndex
 
     orders[index].status = "Pronto"
     return response.json(orders[index])
